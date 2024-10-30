af75a8e98a6f12b68e5b5c908f7100f50939c237
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-10-30 12:58:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ export const instance=new Razorpay({
 const app=express();
 //using middlewares
 app.use(express.json());
-app.use(cors({
-    origin: ['https://elearning-frontend-h0rf77ndi-renugadevis-projects.vercel.app'], // Replace with your actual frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-}));
+app.use(cors);
 
 const port = process.env.PORT;
 
