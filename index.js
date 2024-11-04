ee7bff8dbd170c4d3073ae9be72fcc9b5f9a3edc
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-11-04 23:21:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'https://elearning-front-blond.vercel.app',
+    origin: "https://elearning-front-five.vercel.app",
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
