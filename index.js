e37eb87f84a2ebdc5bb46cbf7551ff36c28deae1
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-11-04 19:41:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ const app=express();
 //using middlewares
 app.use(express.json());
 app.use(cors({
-    origin: ['https://elearning-frontend-topaz.vercel.app'], // Replace with your actual frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
+    origin: ['https://elearning-frontend-topaz.vercel.app'], // Replace with your actual frontend domai
 }));
 
 const port = process.env.PORT;
