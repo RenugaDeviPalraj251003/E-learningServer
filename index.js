906abb15406bc9065d768fbd35b6071a913ccbce
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-11-04 21:48:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ const app=express();
 //using middlewares
 app.use(express.json());
 app.use(cors({
-    origin: "https://elearning-front-navy.vercel.app" // Replace with your actual frontend URL
+    origin: ['https://elearning-front-navy.vercel.app'], // Replace with your actual frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
 }));
-
 
 
 const port = process.env.PORT;
