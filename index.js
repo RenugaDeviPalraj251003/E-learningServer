b0c7ac40af69179eb97f1e5ca68e127f10c36cc6
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-11-05 16:09:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: "https://elearning-front-jet.vercel.app",
+    origin: "https://willowy-mochi-a59eaf.netlify.app",
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
