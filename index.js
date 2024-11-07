f172c32aba4a17e3a6eb1899b384ea94f0698b0a
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-11-07 15:50:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,7 @@ const app=express();
 //using middlewares
 app.use(express.json());
 
-app.use(cors({
-  origin: 'https://iridescent-frangipane-21a186.netlify.app', // Frontend URL
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 
 
 
